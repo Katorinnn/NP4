2c0dcd83cc5445e10b843bd679e8af0f04c6c9aa
KatolinJoi <katlynjoylopez330@gmail.com>
2024-11-20 09:56:45 +0800

    Add project files.

---
+++ LPG Management System/Customers.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Media;
+
+namespace LPG_Management_System
+{
+    internal class Customers
+    {
+        internal static SolidColorBrush Background;
+
+        public Customers()
+        {
+        }
+
+        public static SolidColorBrush Foreground { get; internal set; }
+    }
+}
---
+++ LPG Management System/Login.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LPG_Management_System
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
