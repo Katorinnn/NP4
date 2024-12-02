8e45f428f4335934265300276b8a89e0ffd0d275
KatolinJoi <katlynjoylopez330@gmail.com>
2024-12-02 21:48:40 +0800

    tinulugan

--- LPG Management System/View/UserControls/trySettingsUC.xaml.cs
+++ LPG Management System/View/UserControls/trySettingsUC.xaml.cs
@@ -52,6 +52,11 @@ namespace LPG_Management_System.View.UserControls
                 }
             }
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
 
