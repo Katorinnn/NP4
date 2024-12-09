e9e6bf026569b7be400d52bbd3cee4b4b9ec6322
ja-mendoza <jaynormendoza2023@gmail.com>
2024-12-09 12:16:06 +0800

    wahhhhh

--- LPG Management System/View/Windows/Login.xaml.cs
+++ LPG Management System/View/Windows/Login.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using LPG_Management_System.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace LPG_Management_System
 {
